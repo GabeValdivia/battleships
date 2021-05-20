b0f6fcb41b1c5a0333518a568cb924e589f54b2e
Gabe Valdivia <gabe@gabeswebcoding.com>
2021-05-20 05:14:28 -0500

    Set up needed variables in battleship.js file

--- battleship.js
+++ battleship.js
@@ -1,1 +1,13 @@
-console.log('It works!');
+//Set location of ships
+var location1 = 3;
+var location2 = 4;
+var location3 = 5;
+
+//Set guess, hits and guess count - RANDOMIZE LATER!
+var guess;
+var hits = 0;
+var guesses = 0;
+
+//Determine if ship is sunk
+var isSunk = false;
+
